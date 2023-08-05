4a27a5934f2e4aa724d87b4a669d4b5b61c8b486
brahima <malienk1fry@hotmail.fr>
2023-08-05 15:44:14 +0200

    ✨ F02: LOGIN > add redirection from/to ErrorPage

--- src/components/pages/login/error/ErrorPage.js
+++ src/components/pages/login/error/ErrorPage.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
     <div>
-      <h2>ErrorPage</h2>
+      <h1>ErrorPage</h1> 
+      <Link to="/">
+        <button>Retourner vers la page d'accueil</button>
+      </Link>
     </div>
   )
 }
