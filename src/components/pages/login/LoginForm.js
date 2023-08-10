e99b7fa972e9dfdc08b571dcda3d9db964cb94b2
brahima <malienk1fry@hotmail.fr>
2023-08-10 15:04:12 +0200

    💄F03: LOGIN > LoginForm > input > Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import styled from 'styled-components'
+import { BsPersonCircle } from "react-icons/bs"
 
 
 export default function LoginForm() {
@@ -22,15 +23,80 @@ setInputValue(event.target.value)
 // affichage (render)
   return (
       <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
-      <br/>
-      <h2>Connectez-vous</h2>
-        <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prénom" required/>
-       <button>Accédez à mon espace</button> 
+      <div>
+        <h1>Bienvenue chez nous !</h1>
+
+        <hr/>
+        <h2>Connectez-vous</h2>
+      </div>
+      <div>
+        <div className="input-with-icon" >
+          <BsPersonCircle className="icon"/>  
+          <input
+            value={inputValue}
+            onChange={handleChange} 
+            type="text" 
+            placeholder="Entrez votre prénom" 
+            required/>
+        </div>
+        <button>Accédez à mon espace</button> 
+      </div>  
       </LoginFormStyled>
   )
 }
 
 const LoginFormStyled = styled.div`
- background: green
+ background: green;
+ text-align: center;
+ max-width: 500px;
+ min-width: 400px;
+ margin: 0px auto;
+ padding: 2.5rem 2rem;
+ border-radius: 5px;
+ font-family: "Amatic SC", cursive;
+
+ hr{
+  border: 1.5px solid #f56a2c;
+  margin-bottom: 40px;
+ }
+
+ h1 {
+  color: white;
+  font-size: 48px;
+ }
+
+ h2 {
+  color: #8e8b8b;
+  margin: 20px 10px 10px;
+  color: white;
+  font-size: 36px;
+ }
+
+ .input-with-icon {
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+    }
+
+    &::placeholder {
+      background: white;
+      color: lightgrey;
+    }
+
+ }
 `
