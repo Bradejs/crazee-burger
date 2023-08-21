d1b04cedbb3f19d9b6a39ab9f2ac1aa667a19bf8
brahima <malienk1fry@hotmail.fr>
2023-08-22 00:15:41 +0200

    🚧F04: ORDER > layout > navbar > add left-side & right-side

--- src/components/pages/login/order/Navbar.js
+++ src/components/pages/login/order/Navbar.js
@@ -5,18 +5,29 @@ import styled from 'styled-components'
 export default function Navbar({username}) {
   return (
       <NavbarStyled>
-      Navbar
+        <div className="left-ride">Left</div>
+        <div className="right-side">
+        right
         <h1>Bonjour {username}</h1> 
         <Link to="/">
           <button>Déconnexion</button>
         </Link>
+        </div>  
       </NavbarStyled>
   )
 }
 
 const NavbarStyled = styled.nav`
-   
     background: blue;
     height: 10vh;
-
+    display:flex;
+    justify-content: space-between;
+    /* align-items: center; */
+    
+    .left-side{
+      background: red;
+    }
+    .right-side{
+      background: purple;
+    }
 `
