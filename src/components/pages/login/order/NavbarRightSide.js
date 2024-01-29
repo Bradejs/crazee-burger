a03019786fad3f69c13fc04801974147749dec96
brahima <malienk1fry@hotmail.fr>
2024-01-29 18:56:54 +0100

    ✨ F04: order > navbar > profil crée

--- src/components/pages/login/order/NavbarRightSide.js
+++ src/components/pages/login/order/NavbarRightSide.js
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
+import Profile from "./Profile";
 
 export default function NavbarRightSide({username}) {
-  return  <NavbarRightSidetyled className="right-side">
-  right
-  <h1>Bonjour {username}</h1> 
-  <Link to="/">
-    <button>Déconnexion</button>
-  </Link>
+  return (
+  <NavbarRightSidetyled className="right-side">
+    {/* <div className="admin-button">Admin button</div> */}
+    <Profile />
   </NavbarRightSidetyled> 
+  )
 }
 
 const NavbarRightSidetyled = styled.div`
-      background: purple;
+    
+      display: flex;
+      align-items: center;
+      padding-right: 50px;
+
+      /* .admin-button {
+        background: lightblue;
+      } */
+
+      .profile {
+        background: yellow;
+      }
+
 
 `
       
